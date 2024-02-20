1c486bc784831c34939ec4d437440d391325e51a
saeed <saeed.2fm83@gmail.com>
2024-02-20 15:07:18 +0330

    first page

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        company:
+          "url('https://cdn.kern-liebers.com/fileadmin/user_upload/DE/Unternehmen/unternehmen-footer.jpg')",
+      },
+    },
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
+  },
+  plugins: [],
+};
